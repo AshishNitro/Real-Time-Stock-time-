90b066a8b56c143c3be5b19fe50140bdeaec7032
Äshish sharma <ashishnitro5@gmail.com>
2026-01-03 23:47:00 +0530

    signupFormCreated

--- apps/web/app/(auth)/sign-up/page.tsx
+++ apps/web/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+'"use client";';
+import React, { use } from "react";
+import InputField from "../../../components/page/InputField";
+import { Sign } from "crypto";
 
 const SignUp = () => {
-  const {};
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormInputs>({});
+
   return (
     <>
-      <h1 className="form-title"> Sign & up & Persnlizw</h1>
+      <h1 className="form-title"> Sign Up </h1>
     </>
   );
 };
